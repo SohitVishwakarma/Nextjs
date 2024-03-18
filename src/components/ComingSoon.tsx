873b964baf244af5fba9837561637ce104fd84f0
akash <akash@arivani.net>
2024-03-18 16:05:44 -0400

    first commit

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,13 @@
+const ComingSoon = () => {
+  return (
+    <div className='bg-light dark:bg-primary md:min-w-[calc(100%-120px)] flex items-center justify-center flex-col md:min-h-screen min-h-full text-primary dark:text-white text-[calc(1.2vh+1.2vh+1.2vmin)]'>
+      ArivAni technologies Pvt ltd lucknow.....
+
+      <span>
+Feature Coming Soon 
+      </span>
+    </div>
+  );
+};
+
+export default ComingSoon;
